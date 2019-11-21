4c7645dab0b692a23b9078bf4ee0710e2c8d0626
marcus <marcus.wallen@gmail.com>
2019-11-21 16:25:52 +0100

    Added author info.

--- src/prop/assignment0/Parser.java
+++ src/prop/assignment0/Parser.java
@@ -1,191 +1,197 @@
 package prop.assignment0;
 
+/*
+ * @author Katja Lindeberg
+ * @author Marcus Wallén
+ * @author Jesper Westin
+ */
+
 import java.io.IOException;
 
 public class Parser implements IParser {
-    private ITokenizer tokenizer = null;
+	private ITokenizer tokenizer = null;
 
-    public void open(String fileName) throws IOException, TokenizerException {
-        tokenizer = new Tokenizer();
-        tokenizer.open(fileName);
-        tokenizer.moveNext();
-    }
+	public void open(String fileName) throws IOException, TokenizerException {
+		tokenizer = new Tokenizer();
+		tokenizer.open(fileName);
+		tokenizer.moveNext();
+	}
 
-    public INode parse() throws IOException, TokenizerException, ParserException {
-        if (tokenizer == null)
-            throw new IOException("No open file.");
-        return new AssignNode();
-    }
+	public INode parse() throws IOException, TokenizerException, ParserException {
+		if (tokenizer == null)
+			throw new IOException("No open file.");
+		return new AssignNode();
+	}
 
-    public void close() throws IOException {
-        if (tokenizer != null)
-            tokenizer.close();
-    }
+	public void close() throws IOException {
+		if (tokenizer != null)
+			tokenizer.close();
+	}
 
-    private String addTabs(int tabs) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < tabs; i++){
-            builder.append("\t");
-        }
-        return builder.toString();
-    }
+	private String addTabs(int tabs) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < tabs; i++) {
+			builder.append("\t");
+		}
+		return builder.toString();
+	}
 
-    private class AssignNode implements INode {
-        INode e;
-        Lexeme id;
-        Lexeme ao;
-        Lexeme sc;
+	private class AssignNode implements INode {
+		INode e;
+		Lexeme id;
+		Lexeme ao;
+		Lexeme sc;
 
-        public AssignNode() throws IOException, TokenizerException {
-            id = tokenizer.current();
-            tokenizer.moveNext();
-            ao = tokenizer.current();
-            tokenizer.moveNext();
-            e = new ExprNode();
-            sc = tokenizer.current();
-            tokenizer.moveNext();
-        }
+		public AssignNode() throws IOException, TokenizerException {
+			id = tokenizer.current();
+			tokenizer.moveNext();
+			ao = tokenizer.current();
+			tokenizer.moveNext();
+			e = new ExprNode();
+			sc = tokenizer.current();
+			tokenizer.moveNext();
+		}
 
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            return (String) id.value() + ao.value() + e.evaluate(null) ;
-        }
+		@Override
+		public Object evaluate(Object[] args) throws Exception {
+			return (String) id.value() + ao.value() + e.evaluate(null);
+		}
 
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            builder.append("AssingmentNode\n");
-            String tab = addTabs(++tabs);
-            builder.append(tab + id.toString() + "\n");
-            builder.append(tab + ao.toString() + "\n");
-            builder.append(tab + "ExpressionNode\n");
-            e.buildString(builder, ++tabs);
-            builder.append(tab + sc.toString() + "\n");
+		@Override
+		public void buildString(StringBuilder builder, int tabs) {
+			builder.append("AssingmentNode\n");
+			String tab = addTabs(++tabs);
+			builder.append(tab + id.toString() + "\n");
+			builder.append(tab + ao.toString() + "\n");
+			builder.append(tab + "ExpressionNode\n");
+			e.buildString(builder, ++tabs);
+			builder.append(tab + sc.toString() + "\n");
 
-        }
+		}
 
-    }
+	}
 
-    private class ExprNode implements INode {
+	private class ExprNode implements INode {
 
-        INode t;
-        INode e;
-        Lexeme op;
+		INode t;
+		INode e;
+		Lexeme op;
 
-        public ExprNode() throws IOException, TokenizerException {
-            t = new TermNode();
-            op = tokenizer.current();
-            if (op.token() == Token.ADD_OP || op.token() == Token.SUB_OP){
-                tokenizer.moveNext();
-                e = new ExprNode();
-            }
-        }
+		public ExprNode() throws IOException, TokenizerException {
+			t = new TermNode();
+			op = tokenizer.current();
+			if (op.token() == Token.ADD_OP || op.token() == Token.SUB_OP) {
+				tokenizer.moveNext();
+				e = new ExprNode();
+			}
+		}
 
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            if(op.token() == Token.ADD_OP) {
-               return (double) t.evaluate(null) + (double) e.evaluate(null);
-            } else if (op.token() == Token.SUB_OP){
-                return (double) t.evaluate(null) - (double) e.evaluate(null);
-            } else {
-                return t.evaluate(null);
-            }
-        }
+		@Override
+		public Object evaluate(Object[] args) throws Exception {
+			if (op.token() == Token.ADD_OP) {
+				return (double) t.evaluate(null) + (double) e.evaluate(null);
+			} else if (op.token() == Token.SUB_OP) {
+				return (double) t.evaluate(null) - (double) e.evaluate(null);
+			} else {
+				return t.evaluate(null);
+			}
+		}
 
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            String tab = addTabs(tabs);
-            builder.append(tab + "TermNode\n");
-            t.buildString(builder,++tabs);
-            if (op.token() == Token.ADD_OP || op.token() == Token.SUB_OP) {
-                builder.append(tab + op.toString() + "\n");
-                builder.append(tab + "ExpressionNode\n");
-                e.buildString(builder, ++tabs);
-            }
+		@Override
+		public void buildString(StringBuilder builder, int tabs) {
+			String tab = addTabs(tabs);
+			builder.append(tab + "TermNode\n");
+			t.buildString(builder, ++tabs);
+			if (op.token() == Token.ADD_OP || op.token() == Token.SUB_OP) {
+				builder.append(tab + op.toString() + "\n");
+				builder.append(tab + "ExpressionNode\n");
+				e.buildString(builder, ++tabs);
+			}
 
-        }
+		}
 
-    }
+	}
 
-    private class TermNode implements INode {
+	private class TermNode implements INode {
 
-        INode f;
-        INode t;
-        Lexeme op;
+		INode f;
+		INode t;
+		Lexeme op;
 
-        public TermNode() throws IOException, TokenizerException {
-            f = new FactorNode();
-            op = tokenizer.current();
-            if (op.token() == Token.MULT_OP || op.token() == Token.DIV_OP){
-                tokenizer.moveNext();
-                t = new TermNode();
-            }
-        }
+		public TermNode() throws IOException, TokenizerException {
+			f = new FactorNode();
+			op = tokenizer.current();
+			if (op.token() == Token.MULT_OP || op.token() == Token.DIV_OP) {
+				tokenizer.moveNext();
+				t = new TermNode();
+			}
+		}
 
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            if (op.token() == Token.MULT_OP) {
-                return (double)f.evaluate(null) * (double)t.evaluate(null);
-            } else if (op.token() == Token.DIV_OP) {
-                return (double)f.evaluate(null) / (double)t.evaluate(null);
-            } else {
-                return f.evaluate(null);
-            }
-        }
+		@Override
+		public Object evaluate(Object[] args) throws Exception {
+			if (op.token() == Token.MULT_OP) {
+				return (double) f.evaluate(null) * (double) t.evaluate(null);
+			} else if (op.token() == Token.DIV_OP) {
+				return (double) f.evaluate(null) / (double) t.evaluate(null);
+			} else {
+				return f.evaluate(null);
+			}
+		}
 
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            String tab = addTabs(tabs);
-            builder.append(tab + "FactorNode\n");
-            f.buildString(builder, ++tabs);
-            if (op.token() == Token.MULT_OP || op.token() == Token.DIV_OP) {
+		@Override
+		public void buildString(StringBuilder builder, int tabs) {
+			String tab = addTabs(tabs);
+			builder.append(tab + "FactorNode\n");
+			f.buildString(builder, ++tabs);
+			if (op.token() == Token.MULT_OP || op.token() == Token.DIV_OP) {
 
-                builder.append(tab + op.toString() + "\n");
-                builder.append(tab + "TermNode\n");
-                t.buildString(builder, ++tabs);
-            }
-        }
+				builder.append(tab + op.toString() + "\n");
+				builder.append(tab + "TermNode\n");
+				t.buildString(builder, ++tabs);
+			}
+		}
 
-    }
+	}
 
-    private class FactorNode implements INode {
+	private class FactorNode implements INode {
 
-        INode e;
-        Lexeme firstCh;
-        Lexeme parenRight;
+		INode e;
+		Lexeme firstCh;
+		Lexeme parenRight;
 
-        public FactorNode() throws IOException, TokenizerException {
-            firstCh = tokenizer.current();
-            tokenizer.moveNext();
+		public FactorNode() throws IOException, TokenizerException {
+			firstCh = tokenizer.current();
+			tokenizer.moveNext();
 
-            if (firstCh.token() == Token.LEFT_PAREN) {
-                e = new ExprNode();
-                parenRight = tokenizer.current();
-                tokenizer.moveNext();
-            }
-        }
+			if (firstCh.token() == Token.LEFT_PAREN) {
+				e = new ExprNode();
+				parenRight = tokenizer.current();
+				tokenizer.moveNext();
+			}
+		}
 
-        @Override
-        public Object evaluate(Object[] args) throws Exception {
-            if (firstCh.token() == Token.INT_LIT){
-                return Double.valueOf(firstCh.value().toString());
-            } else if (firstCh.token() == Token.LEFT_PAREN){
-                return e.evaluate(null);
-            }
-            throw new ParserException("Wrong token");
-        }
+		@Override
+		public Object evaluate(Object[] args) throws Exception {
+			if (firstCh.token() == Token.INT_LIT) {
+				return Double.valueOf(firstCh.value().toString());
+			} else if (firstCh.token() == Token.LEFT_PAREN) {
+				return e.evaluate(null);
+			}
+			throw new ParserException("Wrong token");
+		}
 
-        @Override
-        public void buildString(StringBuilder builder, int tabs) {
-            String tab = addTabs(tabs);
-            if (firstCh.token() == Token.INT_LIT) {
-                builder.append(tab + firstCh.toString() + "\n");
-            } else {
-                builder.append(tab + firstCh.toString() + "\n");
-                builder.append(tab + "ExpressionNode\n");
-                e.buildString(builder, ++tabs);
-                builder.append(tab + parenRight.toString() +"\n");
-            }
-        }
-    }
+		@Override
+		public void buildString(StringBuilder builder, int tabs) {
+			String tab = addTabs(tabs);
+			if (firstCh.token() == Token.INT_LIT) {
+				builder.append(tab + firstCh.toString() + "\n");
+			} else {
+				builder.append(tab + firstCh.toString() + "\n");
+				builder.append(tab + "ExpressionNode\n");
+				e.buildString(builder, ++tabs);
+				builder.append(tab + parenRight.toString() + "\n");
+			}
+		}
+	}
 
 }
--- src/prop/assignment0/Tokenizer.java
+++ src/prop/assignment0/Tokenizer.java
@@ -1,97 +1,94 @@
 package prop.assignment0;
 
+/*
+ * @author Katja Lindeberg
+ * @author Marcus Wallén
+ * @author Jesper Westin
+ */
+
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Tokenizer implements ITokenizer {
 
-    private static final Map<Character, Token> symbols = new HashMap<>();
-    private IScanner scanner = null;
-    private Lexeme current = null;
-    private Lexeme next = null;
+	private static final Map<Character, Token> symbols = new HashMap<>();
+	private IScanner scanner = null;
+	private Lexeme current = null;
+	private Lexeme next = null;
 
-    public Tokenizer()
-    {
-        symbols.put('(', Token.LEFT_PAREN);
-        symbols.put(')', Token.RIGHT_PAREN);
-        symbols.put(';', Token.SEMICOLON);
-        symbols.put('+', Token.ADD_OP);
-        symbols.put('-', Token.SUB_OP);
-        symbols.put('*', Token.MULT_OP);
-        symbols.put('/', Token.DIV_OP);
-        symbols.put('=', Token.ASSIGN_OP);
-    }
+	public Tokenizer() {
+		symbols.put('(', Token.LEFT_PAREN);
+		symbols.put(')', Token.RIGHT_PAREN);
+		symbols.put(';', Token.SEMICOLON);
+		symbols.put('+', Token.ADD_OP);
+		symbols.put('-', Token.SUB_OP);
+		symbols.put('*', Token.MULT_OP);
+		symbols.put('/', Token.DIV_OP);
+		symbols.put('=', Token.ASSIGN_OP);
+	}
 
-    @Override
-    public void open(String fileName) throws IOException, TokenizerException {
-        scanner = new Scanner();
-        scanner.open(fileName);
-        scanner.moveNext();
-        next = extractLexeme();
-    }
+	@Override
+	public void open(String fileName) throws IOException, TokenizerException {
+		scanner = new Scanner();
+		scanner.open(fileName);
+		scanner.moveNext();
+		next = extractLexeme();
+	}
 
-    @Override
-    public Lexeme current() {
-        return current;
-    }
+	@Override
+	public Lexeme current() {
+		return current;
+	}
 
-    @Override
-    public void moveNext() throws IOException, TokenizerException {
-        if (scanner == null)
-            throw new IOException("File could not be found");
+	@Override
+	public void moveNext() throws IOException, TokenizerException {
+		if (scanner == null)
+			throw new IOException("File could not be found");
 
-        current = next;
-        if (next.token() != Token.EOF)
-            next = extractLexeme();
-    }
+		current = next;
+		if (next.token() != Token.EOF)
+			next = extractLexeme();
+	}
 
-    private void consumeWhiteSpaces() throws IOException {
-        while (Character.isWhitespace(scanner.current())) {
-            scanner.moveNext();
-        }
-    }
+	private void consumeWhiteSpaces() throws IOException {
+		while (Character.isWhitespace(scanner.current())) {
+			scanner.moveNext();
+		}
+	}
 
-    private Lexeme extractLexeme() throws IOException, TokenizerException {
-        consumeWhiteSpaces();
-        Character currentCharacter = scanner.current();
-        StringBuilder sb = new StringBuilder();
+	private Lexeme extractLexeme() throws IOException, TokenizerException {
+		consumeWhiteSpaces();
+		Character currentCharacter = scanner.current();
+		StringBuilder sb = new StringBuilder();
 
-        if (currentCharacter == Scanner.EOF)
-        {
-            return new Lexeme(currentCharacter, Token.EOF);
-        } else if (Character.isLetter(currentCharacter))
-        {
-            while (Character.isLetter(scanner.current()))
-            {
-                sb.append(scanner.current());
-                scanner.moveNext();
-            }
-            return new Lexeme(sb.toString(), Token.IDENT);
-        } else if (Character.isDigit(currentCharacter))
-        {
-            while (Character.isDigit(scanner.current()))
-            {
-                sb.append(scanner.current());
-                scanner.moveNext();
-            }
-            return new Lexeme(sb.toString(), Token.INT_LIT);
-        } else if (symbols.containsKey(currentCharacter))
-        {
-            scanner.moveNext();
-            return new Lexeme(currentCharacter, symbols.get(currentCharacter));
-        } else
-        {
-            throw new TokenizerException("Unknown character: " + currentCharacter);
-        }
-    }
+		if (currentCharacter == Scanner.EOF) {
+			return new Lexeme(currentCharacter, Token.EOF);
+		} else if (Character.isLetter(currentCharacter)) {
+			while (Character.isLetter(scanner.current())) {
+				sb.append(scanner.current());
+				scanner.moveNext();
+			}
+			return new Lexeme(sb.toString(), Token.IDENT);
+		} else if (Character.isDigit(currentCharacter)) {
+			while (Character.isDigit(scanner.current())) {
+				sb.append(scanner.current());
+				scanner.moveNext();
+			}
+			return new Lexeme(sb.toString(), Token.INT_LIT);
+		} else if (symbols.containsKey(currentCharacter)) {
+			scanner.moveNext();
+			return new Lexeme(currentCharacter, symbols.get(currentCharacter));
+		} else {
+			throw new TokenizerException("Unknown character: " + currentCharacter);
+		}
+	}
 
-    @Override
-    public void close() throws IOException {
-        if (scanner != null)
-        {
-            scanner.close();
-        }
-    }
+	@Override
+	public void close() throws IOException {
+		if (scanner != null) {
+			scanner.close();
+		}
+	}
 
 }
